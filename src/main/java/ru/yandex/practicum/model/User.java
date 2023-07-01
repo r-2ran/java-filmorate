4435dcc6e0c8c86b02202718e2c4b0e296e0e5d9
2ran <brakemire@yandex.ru>
2023-07-01 20:08:33 +0300

    more api fixex

--- src/main/java/ru/yandex/practicum/model/User.java
+++ src/main/java/ru/yandex/practicum/model/User.java
@@ -2,11 +2,13 @@ package ru.yandex.practicum.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private int id;
     private String email;
@@ -14,10 +16,10 @@ public class User {
     private String name;
     private LocalDate birthday;
 
-    public User(int id, String email, String login, LocalDate birthday) {
-        this.id = id;
-        this.email = email;
-        this.login = login;
-        this.birthday = birthday;
-    }
+//    public User(int id, String email, String login, LocalDate birthday) {
+//        this.id = id;
+//        this.email = email;
+//        this.login = login;
+//        this.birthday = birthday;
+//    }
 }
